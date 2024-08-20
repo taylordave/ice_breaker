2e8433797d6fd408af7d1db322f2b324543a6f96
Dave Taylor <ddataylor@gmail.com>
2024-08-20 20:08:02 +0100

    16. LinkedIn Data Processing - Part 4: Custom Search Agent Implmentation

--- agents/linkedin_lookup_agent.py
+++ agents/linkedin_lookup_agent.py
@@ -2,17 +2,51 @@ import os
 from dotenv import load_dotenv
 
 from langchain_ollama import ChatOllama
-from langchain.prompts import PromptTemplate
-from langchain.tools import Tool
+from langchain.prompts.prompt import PromptTemplate
+from langchain_core.tools import Tool
 from langchain.agents import (
     create_react_agent,
     AgentExecutor
 )
 from langchain import hub
 
+from tools.tools import get_profile_url_tavily
 
 load_dotenv()
 
 
 def lookup(name: str) -> str:
-    return "https://www.linkedin.com/in/eden-marco/"
+    llm = ChatOllama(model="mistral")
+
+    template = """
+                   Given the search criteria {name_of_person} I want you to get me a link to their
+                   LinkedIn profile page. Your answer should only contain a URL
+               """
+
+    prompt_template = PromptTemplate(
+        template=template, input_variables=["name_of_person"],
+    )
+
+    tools_for_agent = [
+        Tool(
+            name="Crawl Google 4 linkedin profile page",
+            func=get_profile_url_tavily,
+            description="useful for when you need to get the LinkedIn profile page URL",
+        )
+    ]
+
+    react_prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
+
+    result = agent_executor.invoke(
+        input={"input": prompt_template.format_prompt(name_of_person=name)}
+    )
+
+    linkedin_profile_url = result["output"]
+    return linkedin_profile_url
+
+
+if __name__ == '__main__':
+    linkedin_url = lookup(name="Elon Musk")
+    print(linkedin_url)
