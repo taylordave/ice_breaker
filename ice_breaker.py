a5f8df8eb9cec466f78a2a4f3c982c307cf15c21
Dave Taylor <ddataylor@gmail.com>
2024-08-20 20:21:06 +0100

    17. LinkedIn Data Processing - Part 5: Custom Search Agent Testing

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,17 +4,18 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_ollama import ChatOllama
 
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-if __name__ == "__main__":
-    load_dotenv()
 
-    print("Hello LangChain")
+def ice_break_with(name: str) -> str:
+    linkedin_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_url, mock=True)
 
     summary_template = """
-    given the LinkedIn information {information} about the person I want you to create:
-    1. A short summary
-    2. two interesting facts about them
-    """
+        given the LinkedIn information {information} about the person I want you to create:
+        1. A short summary
+        2. two interesting facts about them
+        """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
@@ -30,3 +31,10 @@ if __name__ == "__main__":
     res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    print("Ice Breaker Enter")
+    ice_break_with(name="Eden Marco Udemy")
+
